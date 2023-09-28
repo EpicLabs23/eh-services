9ec2e7e5aa94c3a6faf74052454576e65bcfc080
Mahmudur Rahman <nahidacm@gmail.com>
2023-09-28 16:19:59 +0600

    fully functional auto login

--- phpmyadmin/config.user.inc.php
+++ phpmyadmin/config.user.inc.php
@@ -13,10 +13,4 @@ $i=0;
 $i++;
 $cfg['Servers'][$i]['auth_type']     = 'signon';
 $cfg['Servers'][$i]['SignonSession'] = 'SignonSession';
-// $cfg['Servers'][$i]['SignonURL']     = 'examples/back-to-ecp.php';
-// $cfg['Servers'][$i]['SignonURL']     = 'examples/custom-signon-url.php';
-
-// $cfg['Servers'][$i]['SignonScript'] = 'examples/custom-signon.php';
-
-// $cfg['Servers'][$i]['LogoutURL'] = 'http://localhost:2325/';
 $cfg['Servers'][$i]['LogoutURL'] = '/ehm-authentication/logout.php';
--- phpmyadmin/examples/back-to-ecp.php
+++
@@ -1,4 +0,0 @@
-<div style="text-align: center;">
-    <h1>What are you doing here? Go home.</h1>
-    <h2>We do not allow direct login.</h2>
-</div>
